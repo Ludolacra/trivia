199d537d851f5ba312d9d6bac40578654b88b460
Dániel Ádám Takács <takacsdanieladam@gmail.com>
2023-07-14 19:36:54 +0200

    minor restructuring in tst makefile added leak detection and address sanitizers added first test for checking the question lists

--- build/config.mk
+++ build/config.mk
@@ -1,6 +1,12 @@
 CXX = g++
-CXX_FLAGS = -std=c++17 -Wall -Wextra # -Werror
+CXX_FLAGS = \
+    -std=c++17 \
+    -Wall \
+    -Wextra \
+    # -Werror
 
 LD = g++
-LD_FLAGS =
+LD_FLAGS = \
+    -lstdc++
+
 LD_LIBRARIES =
---
+++ tst/src/testGame.cpp
@@ -0,0 +1,69 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <memory>
+
+#include <Game/Game.hpp>
+
+using namespace testing;
+
+namespace UT
+{
+    class GameTest : public ::testing::Test
+    {
+    public:
+        std::unique_ptr<Game> testedObject;
+
+        void SetUp()
+        {
+            testedObject = std::make_unique<Game>();
+        }
+        void TearDown()
+        {
+        }
+    };
+
+    TEST_F( GameTest, popQuestionsArePopulatedCorrectly )
+    {
+        ASSERT_EQ( testedObject->popQuestions.size(), 50ul );
+
+        for( unsigned short i = 0; i < 50; ++i )
+        {
+            ASSERT_EQ( testedObject->popQuestions.front(), std::string( "Pop Question " + std::to_string( i ) ) );
+            testedObject->popQuestions.pop_front();
+        }
+    }
+
+    TEST_F( GameTest, scienceQuestionsArePopulatedCorrectly )
+    {
+        ASSERT_EQ( testedObject->scienceQuestions.size(), 50ul );
+
+        for( unsigned short i = 0; i < 50; ++i )
+        {
+            ASSERT_EQ( testedObject->scienceQuestions.front(), std::string( "Science Question " + std::to_string( i ) ) );
+            testedObject->scienceQuestions.pop_front();
+        }
+    }
+
+    TEST_F( GameTest, sportQuestionsArePopulatedCorrectly )
+    {
+        ASSERT_EQ( testedObject->sportsQuestions.size(), 50ul );
+
+        for( unsigned short i = 0; i < 50; ++i )
+        {
+            ASSERT_EQ( testedObject->sportsQuestions.front(), std::string( "Sports Question " + std::to_string( i ) ) );
+            testedObject->sportsQuestions.pop_front();
+        }
+    }
+
+    TEST_F( GameTest, rockQuestionsArePopulatedCorrectly )
+    {
+        ASSERT_EQ( testedObject->rockQuestions.size(), 50ul );
+
+        for( unsigned short i = 0; i < 50; ++i )
+        {
+            ASSERT_EQ( testedObject->rockQuestions.front(), std::string( "Rock Question " + std::to_string( i ) ) );
+            testedObject->rockQuestions.pop_front();
+        }
+    }
+}
