01a4024a764664d6a6afddd37bdc19d28a22d52f
Dániel Ádám Takács <takacsdanieladam@gmail.com>
2023-07-15 10:59:32 +0200

    fixed coverage and sanitizers added tests for Game::currentCategory() (in a separate commit, because the refactored code will look really similar to the test implementation)

--- tst/src/testGame.cpp
+++ tst/src/testGame.cpp
@@ -3,7 +3,8 @@
 
 #include <memory>
 
-#include <Game/Game.hpp>
+#include "Game/Game.hpp"
+#include "Game/Topic.hpp"
 #include "mock/QuestionFactory.hpp"
 
 using namespace testing;
@@ -19,7 +20,8 @@ namespace UT
         void SetUp()
         {
             questionFactoryMock = std::make_shared<UT::Mock::QuestionFactory>();
-            EXPECT_CALL( *questionFactoryMock, generateQuestions( _, _ ) ).Times( 4 );
+            EXPECT_CALL( *questionFactoryMock, generateQuestions ).Times( 4 );
+
             testedObject = std::make_unique<Game>( questionFactoryMock );
         }
         void TearDown()
@@ -29,7 +31,42 @@ namespace UT
         }
     };
 
-    TEST_F( GameTest, qustionsAreGenerated )
+    TEST( GameQuestionGenerationTest, qustionsAreGenerated )
     {
+        auto questionFactoryMock = std::make_shared<UT::Mock::QuestionFactory>();
+        EXPECT_CALL( *questionFactoryMock, generateQuestions( Topic::Pop, 50 ) );
+        EXPECT_CALL( *questionFactoryMock, generateQuestions( Topic::Science, 50 ) );
+        EXPECT_CALL( *questionFactoryMock, generateQuestions( Topic::Sports, 50 ) );
+        EXPECT_CALL( *questionFactoryMock, generateQuestions( Topic::Rock, 50 ) );
+
+        auto testedObject = std::make_unique<Game>( questionFactoryMock );
+    }
+
+    TEST_F( GameTest, questionCategoryIsDeterminedBasedOnPlayerLocaion )
+    {
+        testing::internal::CaptureStdout();
+        testedObject->add( "Bob" );
+        testing::internal::GetCapturedStdout();
+
+        constexpr const unsigned short categoryCount = 4;
+        for( unsigned short i = 0; i <= 11; ++i )
+        {
+            testedObject->places[testedObject->currentPlayer] = i;
+            switch( i % categoryCount )
+            {
+                case 0:
+                    ASSERT_EQ( "Pop", testedObject->currentCategory() );
+                    break;
+                case 1:
+                    ASSERT_EQ( "Science", testedObject->currentCategory() );
+                    break;
+                case 2:
+                    ASSERT_EQ( "Sports", testedObject->currentCategory() );
+                    break;
+                case 3:
+                    ASSERT_EQ( "Rock", testedObject->currentCategory() );
+                    break;
+            }
+        }
     }
 }
