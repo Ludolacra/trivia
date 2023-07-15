da0985bfa154a20e1865006b8e0e4e9d8f75c48a
Dániel Ádám Takács <takacsdanieladam@gmail.com>
2023-07-15 11:35:21 +0200

    refactored Game::currentCategory() logic

--- src/Game.cpp
+++ src/Game.cpp
@@ -113,25 +113,14 @@ void Game::askQuestion()
 
 string Game::currentCategory()
 {
-    if( places[currentPlayer] == 0 )
-        return "Pop";
-    if( places[currentPlayer] == 4 )
-        return "Pop";
-    if( places[currentPlayer] == 8 )
-        return "Pop";
-    if( places[currentPlayer] == 1 )
-        return "Science";
-    if( places[currentPlayer] == 5 )
-        return "Science";
-    if( places[currentPlayer] == 9 )
-        return "Science";
-    if( places[currentPlayer] == 2 )
-        return "Sports";
-    if( places[currentPlayer] == 6 )
-        return "Sports";
-    if( places[currentPlayer] == 10 )
-        return "Sports";
-    return "Rock";
+    constexpr const unsigned short categoryCount = 4;
+    switch( places[currentPlayer] % categoryCount )
+    {
+        case 0: return "Pop";
+        case 1: return "Science";
+        case 2: return "Sports";
+        default: return "Rock";
+    }
 }
 
 bool Game::wasCorrectlyAnswered()
